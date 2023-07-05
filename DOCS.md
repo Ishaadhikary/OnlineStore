d58e143d4bbc692ad40b7c23851eff5e075ba05f
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-07-05 20:31:20 +0545

    Adding the first container-nav bar

---
+++ DOCS.md
@@ -0,0 +1,15 @@
+# Technologies Used:
+ ## React
+ ## JS
+ ## HTML
+ ## CSS
+ ## Vite
+
+# Installation Process:
+ ## Clone the project
+ ## Install node (If not installed)
+ ### npm install
+
+# for icons
+ ## npm install react-icons --save
+ 
--- OnlineStoreApp/src/assets/components/FirstContainer.jsx
+++ OnlineStoreApp/src/assets/components/FirstContainer.jsx
@@ -1,5 +1,23 @@
-export default function FirstContainer(){
-    return(
-        <>First Container</>
-    )
+// Nav Bar Container
+import { AiOutlineSearch } from "react-icons/ai";
+import "../css/firstContainer.css";
+
+export default function FirstContainer() {
+  return (
+    <>
+      <div class="first-container">
+        <img src="/logo.svg" alt="" />
+        <span className="first-container-second-item">
+            <a href="#" >Products</a>
+            <a href="#">Categories</a>
+            <a href="#">What's New</a>
+          </span>
+          <span class="first-container-third-item">
+            <input type="text" name="search" id="search" placeholder="Search" />{" "}
+            <AiOutlineSearch />
+          </span>
+
+      </div>
+    </>
+  );
 }
