d965d8a72011ce8f7bd5132f1de76adb1af1cfe3
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-07-05 21:34:24 +0545

    Adding second container- offer section

--- OnlineStoreApp/src/assets/components/SecondContainer.jsx
+++ OnlineStoreApp/src/assets/components/SecondContainer.jsx
@@ -1,5 +1,16 @@
-export default function SecondContainer(){
-    return(
-        <>Second Container</>
-    )
+import "../css/secondContainer.css";
+export default function SecondContainer() {
+  return (
+    <>
+      <div className="second-container">
+        <div className="second-container-text">
+          <h1>LET’S EXPLORE</h1> <h1 className="second-container-center-text">UNIQUE</h1><h1> CLOTHES.</h1>
+          <div className="second-container-bottom-text"> <img src="/secondContainerVector.svg" alt="" /><button className="seconde-container-button">Shop Now</button></div>
+        </div>
+        <div className="second-container-img">
+          <img src="/secondContainerImg.svg" alt="" />
+        </div>
+      </div>
+    </>
+  );
 }
