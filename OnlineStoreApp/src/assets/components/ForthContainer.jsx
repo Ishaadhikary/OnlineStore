d76950ba6ecaa96f514514e67b3a76162d2093de
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-07-06 11:49:30 +0545

    Adding the data from the fakeapi in the cards

--- OnlineStoreApp/src/assets/components/ForthContainer.jsx
+++ OnlineStoreApp/src/assets/components/ForthContainer.jsx
@@ -1,11 +1,43 @@
-import Card from './Card';
-import '../css/forth-container.css'
+import React, { useState, useEffect } from "react";
+// import Card from "./Card";
+import "../css/forthContainer.css";
 export default function ForthContainer() {
+  const [updateStoreData, setUpdateStoreData] = useState([]);
+  useEffect(() => {
+    storeData();
+  }, []);
+  const storeData = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    console.log(response);
+    const jasonData = await response.json();
+    console.log(jasonData);
+    setUpdateStoreData(jasonData);
+  };
   return (
     <>
       <div className="forth-container">
-        <h2>Products<hr/></h2>
-        <Card/>
+        <h2>
+          Products
+          <hr />
+        </h2>
+        <div className="card-container-box">
+          {updateStoreData.map((values) => {
+            return (
+              <>
+                <div className="card-container">
+                  <div>
+                    <img src="/secondContainerImg.svg" alt="" />
+                    <p>{`$ ${values.price}`}</p>
+                    <h4>{values.title}</h4>
+                    <p>{values.description}</p>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+
+          <button>View All</button>
+        </div>
       </div>
     </>
   );
