980d1abdd55bad7ca2038c20735bb8ddb7152888
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-07-06 12:54:53 +0545

    Updating the css for the cards

--- OnlineStoreApp/src/assets/components/ForthContainer.jsx
+++ OnlineStoreApp/src/assets/components/ForthContainer.jsx
@@ -27,10 +27,11 @@ export default function ForthContainer() {
               <>
                 <div className="card-container">
                   <div>
-                    <img src={values.image} alt="" />
+                    <img className="product-img" src={values.image} alt="" />
                     <p><strong>{`$ ${values.price}`}</strong></p>
                     <span className="second-container-second-item"><img src="/star.svg" alt="Rating Star" /> {`${values.rating.rate}(${values.rating.count})`}</span>
                     <h4>{values.title}</h4>
+                    <p>{values.description}</p>
                   </div>
                 </div>
               </>
